4923fe1cd1aa2507c00945f696e89cbdda73cb25
Ayan <support@aanvy.com>
2019-08-12 23:50:39 +0530

    Updates

---
+++ precache-manifest.d97d634bd3f29976084c2423ea868be3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d651898dd375c45b97ccb9f9c46887f",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "0b6b76ba93ed8b495083",
+    "url": "/memegenerator/static/css/main.2c2a49c3.chunk.css"
+  },
+  {
+    "revision": "bc465914a10bac6ad46b",
+    "url": "/memegenerator/static/js/2.3770382e.chunk.js"
+  },
+  {
+    "revision": "0b6b76ba93ed8b495083",
+    "url": "/memegenerator/static/js/main.cc6f14fc.chunk.js"
+  },
+  {
+    "revision": "233397c311cf934d4768",
+    "url": "/memegenerator/static/js/runtime~main.e001f63e.js"
+  }
+]);
